b43332f3905870ffecad444cac5225ebd32f309b
Lily3214 <111310939+Lily3214@users.noreply.github.com>
2023-05-22 22:57:26 +0900

    Add files via upload

---
+++ adv-dealership-project2/src/main/java/com/yearup/dealership/Contract.java
@@ -0,0 +1,17 @@
+package com.yearup.dealership;
+
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+
+}
